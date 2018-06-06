f3ed2073cd0c9128545c0ad8089d5c0b37338a41
Jack <jacklooi@users.noreply.github.com>
2018-06-06 15:00:49 +0800

    update app.js with static public folder

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ const route = require('./route');
 const app = express();
 
 app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
 app.use('/', route);
 
-var port = process.env.PORT || 8000;
+var port = process.env.PORT || 3000;
 app.listen(port, function() {
     console.log('Shortener service listening on port ' + port);
 });
